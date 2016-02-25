20419bcc986c77d524d742ef8d263660b7e25870
yushengc <ntuyusheng@gmail.com>
2016-02-26 00:36:48 +0800

    groups, and group

--- app/controllers/cgroups_controller.rb
+++ app/controllers/cgroups_controller.rb
@@ -2,4 +2,7 @@ class CgroupsController < ApplicationController
 	def index
 		@groups = Mgroup.all
 	end
+	def new
+		@groups = Mgroup.new
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 root 'cgroups#index'
-resources:cgroups
+resources :cgroups
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
