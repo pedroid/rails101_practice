f3c677266ef8f0b48947cec56ebbbf038221a733
yushengc <ntuyusheng@gmail.com>
2016-02-27 01:02:23 +0800

    post related setting(model, controller, routes)

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 root 'groups#index'
-resources :groups
+#resources :groups
+resources :groups do
+	resources :posts
+end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160226101151) do
+ActiveRecord::Schema.define(version: 20160226165948) do
 
   create_table "groups", force: :cascade do |t|
     t.datetime "created_at",  null: false
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 20160226101151) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
