accd1865d581d0944b14aa8c6259cb727d9f030d
yushengc <ntuyusheng@gmail.com>
2016-02-26 18:04:37 +0800

    new action

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-root 'cgroups#index'
-resources :cgroups
+root 'groups#index'
+resources :groups
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160225144450) do
+ActiveRecord::Schema.define(version: 20160225163622) do
+
+  create_table "groups", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "m_groups", force: :cascade do |t|
     t.string   "title"
