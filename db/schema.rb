e8b86fb98b8da8598939650bc79994f9e966bf57
yushengc <ntuyusheng@gmail.com>
2016-02-28 23:00:00 +0800

    user name added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160228133522) do
+ActiveRecord::Schema.define(version: 20160228145009) do
 
   create_table "groups", force: :cascade do |t|
     t.datetime "created_at",  null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20160228133522) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
